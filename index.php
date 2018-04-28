7fe453469c95b5880df56dda1543968da5a21938
FranciscoCastilloCastillo <fmanuelcastillo@hotmail.com>
2018-04-28 20:41:20 +0200

    mensaje de inicio

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DWES</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+  <h2>DESPLIEGUE DE APLICACIONES WEB - UNIDAD 6 -CASO PRACTICO 1</h2>
+  <fieldset>
+    <form action="" method="post">
+      <label>Usuario</label></br>
+      <input type="text" id="usuario" name="usuario" required></br>
+      <label>Contraseña</label></br>
+      <input type="password" id="password" name="password" required></br></br>
+      <input type="submit" class="w3-button w3-black"value="ENTRAR">
+    </form></br>
+    <a href="registro.php"><b>Registrarse</b></a></br>
+  </fieldset>
+  <footer>
+    <h6>Alumno: Francisco Manuel Castillo Castillo - Asignatura: DAW</h6>
+  </footer>
+  </body>
+</html>
